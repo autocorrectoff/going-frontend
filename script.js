9f5b345772123d97f92faf03c2db92ff385565dc
LAPTOP-QLEGTRSJ\radio <branko.miric.ns@gmail.com>
2023-11-07 17:19:09 +0100

    lets log balance

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 $(document).ready(function () {
+    let signer;
     $("#connect").click(async function () {
-        const signer = await connectWallet();
-        console.log(signer.address);
+        signer = await connectWallet();
+        console.log(`Connected with ${signer.address}`);
+    });
+    $("#balance").click(async function () {
+        const usdcContract = getUsdcContractInstance(signer);
+        const balance = await usdcContract.balanceOf(signer.address);
+        const decimals = await usdcContract.decimals();
+        const balanceParsed = ethers.formatUnits(balance, decimals);
+        console.log(`USDC balance of connected wallet: ${balanceParsed}`);
     });
 });
 
 const provider = new ethers.BrowserProvider(window.ethereum);
+const usdcAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
 
 async function connectWallet() {
     if (window.ethereum) {
@@ -14,4 +23,8 @@ async function connectWallet() {
     } else {
         console.error("Please Install Metamask!!!");
     }
+}
+
+function getUsdcContractInstance(signer) {
+    return new ethers.Contract(usdcAddress, usdcABI, signer);
 }
